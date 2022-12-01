f53f8324a3b0e2830a17d44eb715811f594ef6b8
digital <digital@iMac24-7.local>
2022-12-01 13:23:16 +0100

    init

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by digital on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
